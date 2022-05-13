41d2ca718d59eb32468f0045588a08b43bdc4c76
Alexander Khlyntcev <khlyntcevsasha@gmail.com>
2022-05-13 12:40:22 +0300

    Solutions homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,12 @@ import ru.netology.sender.MessageSenderImpl;
 
 public class Main {
 
-    //Тестовый пример
     public static void main(String[] args) {
         GeoService geoService = new GeoServiceImpl();
         LocalizationService localizationService = new LocalizationServiceImpl();
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
 
-        Map<String, String> headers = new HashMap<String, String>();
+        Map<String, String> headers = new HashMap<>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
         messageSender.send(headers);
     }
